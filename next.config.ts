d96521385f038a507cd63aa3fa101491ded92306
Padreeca <g.padrecanicoletti@gmail.com>
2025-07-14 19:02:02 -0300

    initial page idea

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,15 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  //   async redirects() {
+  //   return [
+  //     {
+  //       source: '/:path((?!api/).*)', // Corresponde a TUDO, exceto a página inicial e rotas de API
+  //       destination: '/', // Redireciona para a página inicial
+  //       permanent: false, // Use 'false' pois é um redirecionamento temporário
+  //     },
+  //   ]
+  // },
 };
 
 export default nextConfig;
